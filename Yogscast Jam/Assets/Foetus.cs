2a87020ef84d195c9cbb910ba40a4215e6b32f97
Topy <Clement.Orlandini.Etu@univ-lemans.fr>
2019-12-14 12:54:45 +0100

    Foetus can consume resources and create waste
    It can also gain mass

---
+++ Yogscast Jam/Assets/Foetus.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Foetus : MonoBehaviour
+{
+    public float health = 100f;
+    public float food = 100f;
+    public float oxygen = 100f;
+    public float waste = 0f;
+    public float carbonDioxyde = 0f;
+    public float mass = 0f;
+
+    public Slider oxygenBar;
+    public Slider wasteBar;
+    public Slider carbonDioxydeBar;
+    public Slider healthBar;
+    public Slider massBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateStats", 0, 1f);
+    }
+
+    void FixedUpdate()
+    {
+        // Display bar values
+        oxygenBar.value = oxygen;
+        healthBar.value = health;
+        wasteBar.value = waste;
+        carbonDioxydeBar.value = carbonDioxyde;
+        massBar.value = mass;
+
+    }
+
+    void UpdateStats()
+    {
+        food -= 1;
+        oxygen -= 1;
+
+        if (food > 0)
+            waste++;
+        if (food > 70)
+            mass++;
+        if (food < 20)
+        {
+            mass--;
+            health--;
+        }
+
+        if (oxygen > 0)
+            carbonDioxyde++;
+        if (oxygen <= 0)
+            health-= 10;
+        if (carbonDioxyde >= 90)
+            health -= 5;
+
+        oxygen = Mathf.Clamp(oxygen, 0, 100);
+        food = Mathf.Clamp(food, 0, 100);
+        mass = Mathf.Clamp(mass, 0, 100);
+        health = Mathf.Clamp(health, 0, 100);
+        waste = Mathf.Clamp(waste, 0, 100);
+        carbonDioxyde = Mathf.Clamp(carbonDioxyde, 0, 100);
+    }
+}
